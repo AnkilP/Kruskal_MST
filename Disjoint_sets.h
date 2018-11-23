25d3bca05e991de1e5c3dfde641a6f46adf4a553
AnkilP <a293pate@uwaterloo.ca>
2018-11-23 15:44:07 -0500

    initial commit

---
+++ Disjoint_sets.h
@@ -0,0 +1,95 @@
+
+#ifndef DISJOINT_SET_H
+#define DISJOINT_SET_H
+
+#ifndef nullptr
+#define nullptr 0
+#endif
+
+
+#include <limits>
+#include "Exception.h"
+
+using namespace std;
+
+//struct ll_entry; // represents each element of the linked list
+//struct set_info; // keeps track of the information (pointers to head and tail and the size of the set) of each set
+//could we delete the above two lines?
+
+
+struct ll_entry{ 
+	int content;
+	set_info* ptr_to_info; // ptr to the info entry of the corresponding set
+	ll_entry* next;
+	
+};
+
+struct set_info { 
+	ll_entry* head;
+	ll_entry* tail;
+	int size;
+};
+
+class Disjoint_set {
+
+private:
+	ll_entry** nodes;
+	set_info** sets;
+	int set_counter;
+	int initial_num_sets;
+public:
+	Disjoint_set(int);
+	~Disjoint_set();
+	int find_set(int) const;
+	int num_sets() const;
+	void union_sets(int, int);
+};
+
+Disjoint_set::Disjoint_set(int n) : nodes(new ll_entry*[n]), 
+sets (new set_info*[n]), 
+set_counter(n),
+initial_num_sets(n) {
+	// initialize the sets	
+}
+
+Disjoint_set::~Disjoint_set() {
+	// deallocate memory
+}
+int Disjoint_set::find_set(int item) const{
+	// should it be? return nodes[item]->ptr_to_info->head->content 
+	return 0;
+}
+
+int Disjoint_set::num_sets() const {
+	return set_counter;
+}
+
+void Disjoint_set::union_sets(int node_index1, int node_index2) {
+	
+	if (node_index1 == node_index2)
+		return;
+	
+	set_info* si1 = nodes[node_index1]->ptr_to_info;
+	set_info* si2 = nodes[node_index2]->ptr_to_info;
+
+	// ni1: the index of the larger set, ni2: the index of the smaller index
+	int ni1 = si1->size >= si2->size ? node_index1 : node_index2; 
+	int ni2 = si1->size < si2->size ? node_index1 : node_index2;
+
+
+	/* iterate through the shorter list and modify the pointers
+	while (has not reached the end of set) {
+		....
+	}*/
+
+	// do we need to modify anything else?
+	
+
+	// delete the set_info entry that no longer exists
+	
+	
+	
+}
+
+
+#endif
---
+++ Weighted_graph.h
@@ -0,0 +1,174 @@
+
+/*****************************************
+ * Instructions
+ *  - Replace 'uwuserid' with your uWaterloo User ID
+ *  - Select the current calendar term and enter the year
+ *  - List students with whom you had discussions and who helped you
+ *
+ * uWaterloo User ID:  a293pate@edu.uwaterloo.ca
+ * Submitted for ECE 250
+ * Department of Electrical and Computer Engineering
+ * University of Waterloo
+ * Calender Term of Submission:  Fall 2019
+ *
+ * By submitting this file, I affirm that
+ * I am the author of all modifications to
+ * the provided code.
+ *
+ * The following is a list of uWaterloo User IDs of those students
+ * I had discussions with in preparing this project:
+ *    -
+ *
+ * The following is a list of uWaterloo User IDs of those students
+ * who helped me with this project (describe their help; e.g., debugging):
+ *    -
+ *****************************************/
+
+#ifndef WEIGHTED_GRAPH_H
+#define WEIGHTED_GRAPH_H
+
+#ifndef nullptr
+#define nullptr 0
+#endif
+
+#include <iostream>
+#include <limits>
+#include <vector>
+#include "Exception.h"
+#include "Disjoint_sets.h"
+
+using namespace std;
+
+class Weighted_graph {
+	private:
+		static const double INF;
+		double** graph;
+		int num_nodes;
+		int num_edges;
+
+		// Do not implement these functions!
+		// By making these private and not implementing them, any attempt
+		// to make copies or assignments will result in errors
+		Weighted_graph( Weighted_graph const & );
+		Weighted_graph &operator=( Weighted_graph );
+
+		// your choice
+
+	public:
+		Weighted_graph( int = 10 );
+		~Weighted_graph();
+
+		int degree( int ) const;
+		int edge_count() const;
+		std::pair<double, int> minimum_spanning_tree() const;
+
+		bool insert_edge( int, int, double );
+		bool erase_edge( int, int );
+		void clear_edges();
+		bool edge_exists(int i, int j);
+
+	// Friends
+
+	friend std::ostream &operator<<( std::ostream &, Weighted_graph const & );
+};
+
+const double Weighted_graph::INF = std::numeric_limits<double>::infinity();
+
+Weighted_graph::Weighted_graph(int n ) {
+	graph = new double*[n];
+	for(int i = 0; i < n; ++i){
+		graph[i] = new double[n];
+		for(int j = 0; j < n; ++j){
+			graph[i][j] = INF;
+		}
+	} //this is a matrix not a list
+	num_edges = 0;
+	num_nodes = n;
+}
+
+Weighted_graph::~Weighted_graph() {
+	for(int i = 0; i < num_nodes; ++i){
+		delete[] graph[i];
+	}
+	delete[] graph; 
+}
+
+int Weighted_graph::degree(int u) const {
+	int count = 0;
+	for(int i =0; i < num_nodes; ++i){
+		if(graph[u][i] != INF){
+			count++;
+		}
+	}
+	return count;
+}
+
+int Weighted_graph::edge_count() const {
+	return num_edges;
+}
+
+bool Weighted_graph::erase_edge(int i, int j) {
+	if(edge_exists(i,j) == false){
+		return false;
+	}
+	graph[i][j] = INF;
+	graph[j][i] = INF;
+
+	--num_edges;
+	return true;
+}
+
+void Weighted_graph::clear_edges() {
+	for(int i = 0; i < num_nodes; ++i){
+		for(int j = 0; j < num_nodes; ++j){
+			graph[i][j] = INF;
+		}
+	}
+	
+}
+
+bool Weighted_graph::insert_edge( int i, int j, double d ) {
+	if(i > num_nodes - 1 || i < 0 || j > num_nodes - 1 || j < 0 || d <= 0){
+		throw illegal_argument();
+	}
+
+	if(i == j && edge_exists(i,j)){
+		return false;
+	}
+
+	graph[i][j] = d;
+	graph[j][i] = d;
+
+	++num_edges;
+	return true;
+}
+
+bool Weighted_graph::edge_exists(int i, int j){
+	//Assumption: since the graph is undirected, 
+	//if I find an edge going in one direction, 
+	//then the arc going in the other direction also exists
+
+	if(graph[i][j] == INF){
+		return false;
+	}
+	return true;
+
+}
+
+std::pair<double, int> Weighted_graph::minimum_spanning_tree() const {
+	Disjoint_set ds(this->num_nodes);
+
+	
+	
+
+	return std::pair<double, int>( 0.0, 0 );
+}
+
+std::ostream &operator<<( std::ostream &out, Weighted_graph const &graph ) {
+	
+
+	return out;
+}
+
+
+#endif
